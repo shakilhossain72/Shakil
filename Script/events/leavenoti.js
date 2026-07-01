f9ef5b34cf87696a34112bd30c43c9165acbc4b6
shakilhossain72 <n7733887@gmail.com>
2026-07-01 21:49:49 +0600

    Update leavenoti.js

--- Script/events/leavenoti.js
+++ Script/events/leavenoti.js
@@ -24,7 +24,7 @@ module.exports.run = async function({ api, event, Users, Threads }) {
     ? " তোর সাহস কম না  গ্রুপের এডমিনের পারমিশন ছাড়া তুই লিভ  নিস😡😠🤬 \n✦─────꯭─⃝‌‌𝐒𝐡𝐚kil Hossain────✦"
     : "তোমার এই গ্রুপে থাকার কোনো যোগ্যাতা নেই ছাগল😡\nতাই তোমাকে লাথি মেরে গ্রুপ থেকে বের করে দেওয়া হলো🤪 WELLCOME REMOVE🤧\n✦─────꯭─⃝‌‌Shakil Hossain────✦";
 
-  const path = join(__dirname, "Shahadat", "leaveGif");
+  const path = join(__dirname, "Shakil", "leaveGif");
   const gifPath = join(path, `leave1.gif`);
 
   if (!existsSync(path)) mkdirSync(path, { recursive: true });
